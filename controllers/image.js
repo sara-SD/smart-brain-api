d8a4a3deae11d85c0d1df95d6c575076dbf67669
Sara Sadeghian <sara.sadeghian.t@gmail.com>
2019-06-02 18:12:25 +0430

    adding smart-brain-api

---
+++ controllers/image.js
@@ -0,0 +1,25 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '19c771ca0d77488aa4caee8cb8b6fe13'
+   });
+  
+const handleApiCall = (req, res) =>{
+    app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => res.json(data))
+    .catch(err => res.status(400).json('Unable to work with api'));
+   }
+   
+const handleImage = (req, res, db)=>{
+    const {id} = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => res.json(entries[0]))
+    .catch(err => res.status(400).json('unable to get entries'))
+}
+module.exports = {
+    handleImage: handleImage,
+    handleApiCall: handleApiCall
+}
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex  = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'test',
+    database : 'smartbrain'
+  }
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) =>{ res.json(database.users)});
+app.post('/signin',(req, res) =>{ signin.handleSignIn(req, res, db, bcrypt)});
+app.post('/register', (req, res) =>{ register.handleRegister(req, res ,db, bcrypt)} );
+app.get('/profile/:id', (req, res) =>{ profile.handleProfileGet(req, res, db)} );
+app.put('/image', (req, res) =>{ image.handleImage(req, res, db)});
+app.post('/imageUrl', (req, res) =>{ image.handleApiCall(req, res)});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () =>{
+    console.log(`server is running on port ${PORT}`);
+});
